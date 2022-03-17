86361bf1661cf9c06fa4419dfc9432b32c70d506
YeonBae Park <makemakeway@gmail.com>
2022-03-17 23:38:21 +0900

    알고리즘

---
+++ MaxConsecutiveOnes.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+/*
+ 
+ Given a binary array nums, return the maximum number of consecutive 1's in the array.
+ 
+ */
+
+class Solution {
+    func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
+        var answer: Int = 0
+
+        
+        return answer
+    }
+}
