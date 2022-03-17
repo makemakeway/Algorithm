403fb677ace000ea985d9325de0a2f2056c38035
makemakeway <makemakeway@gmail.com>
2022-03-18 04:08:42 +0900

    알고리즘

--- MaxConsecutiveOnes.playground/Contents.swift
+++ MaxConsecutiveOnes.playground/Contents.swift
@@ -7,10 +7,40 @@ import UIKit
  */
 
 class Solution {
+    var cnt: Int = 0
+    var tmp: [Int] = []
+    
     func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
-        var answer: Int = 0
-
+        if !(checkContainsOne(nums)) {
+            return 0
+        }
+        for num in nums {
+            if num == 1 {
+                tmp.append(num)
+            } else {
+                updateCount()
+                tmp.removeAll()
+            }
+        }
         
-        return answer
+        if !(tmp.isEmpty) {
+            updateCount()
+        }
+        
+        return cnt
     }
+    
+    func updateCount() {
+        if cnt < tmp.count {
+            cnt = tmp.count
+        }
+    }
+    
+    func checkContainsOne(_ nums: [Int]) -> Bool {
+        if nums.contains(1) {
+            return true
+        }
+        return false
+    }
+    
 }
