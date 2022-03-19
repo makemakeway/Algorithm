3ebe4bbae8c82ef37c623b4ad43b45d0dd7ab8f1
makemakeway <makemakeway@gmail.com>
2022-03-19 18:40:11 +0900

    알고리즘

--- FindNumbersWithEvenNumberOfDigits.playground/Contents.swift
+++ FindNumbersWithEvenNumberOfDigits.playground/Contents.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class Solution {
     func findNumbers(_ nums: [Int]) -> Int {
+        var answer = 0
         
+        for num in nums {
+            if checkDigits(num) {
+                answer += 1
+            }
+        }
+        
+        return answer
+    }
+    
+    func checkDigits(_ num: Int) -> Bool {
+        let digits = String(num)
+        
+        if digits.count % 2 == 0 {
+            return true
+        }
+        
+        return false
     }
 }
